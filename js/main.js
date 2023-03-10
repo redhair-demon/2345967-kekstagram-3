4b2f896bd2513a68105f20d11a1241444099351d
naego <naegorov@hotmail.com>
2023-03-10 17:04:32 +0300

    max_length -> maxLength

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ function getRandInt(from, to) {
   return Math.floor(Math.min(from) + Math.random() * Math.abs(to-from));
 }
 
-function isStringFits(string, max_length) {
-  return string.length <= max_length;
+function isStringFits(string, maxLength) {
+  return string.length <= maxLength;
 }
