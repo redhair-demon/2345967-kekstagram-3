d9bc0e2609461f91caac718c2d73d2ba9aad2883
naego <naegorov@hotmail.com>
2023-03-10 17:07:37 +0300

    function usage

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ function getRandInt(from, to) {
   return Math.floor(Math.min(from) + Math.random() * Math.abs(to-from));
 }
 
+getRandInt(31, 209);
+
 function isStringFits(string, maxLength) {
   return string.length <= maxLength;
 }
+
+isStringFits('helloworld', 9);
