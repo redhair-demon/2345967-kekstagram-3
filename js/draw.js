fcda209406d19b9a024ab10964027fad213d6614
naego <naegorov@hotmail.com>
2023-03-31 14:39:40 +0300

    Отрисуй меня полностью часть 1

---
+++ js/draw.js
@@ -0,0 +1,19 @@
+import { getPhotos } from './data.js';
+
+const pictureTemplate = document.querySelector('#picture').content;
+const pictures = document.querySelector('.pictures');
+const newFragment = new DocumentFragment();
+
+getPhotos().forEach((photo)=> {
+  const template = pictureTemplate.cloneNode(true);
+  const img = template.querySelector('.picture__img');
+  const likes = template.querySelector('.picture__likes');
+  const comments = template.querySelector('.picture__comments');
+  img.src = photo.url;
+  likes.textContent = photo.likes;
+  comments.textContent = photo.comments;
+  newFragment.appendChild(template);
+});
+
+pictures.appendChild(newFragment);
+export {pictures};
